09a8175652284781c15fbb0f0836b3b381dad004
LiquidLemonLOL <oscar.wallin96@hotmail.com>
2026-09-09 11:16:49 +0200

    trying init setup with Spring Initializr

--- src/main/java/se/lexicon/Customer.java
+++ src/main/java/se/lexicon/Customer.java
@@ -1,7 +1,17 @@
 package se.lexicon;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Customer {
 
 
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
 
 }
